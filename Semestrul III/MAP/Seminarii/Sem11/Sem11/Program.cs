d1e6cd31f2e14b49ccfec6d43e7aceed568fdcc2
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2023-12-14 00:46:19 +0200

    MAP seminar 11

---
+++ Semestrul III/MAP/Seminarii/Sem11/Sem11/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Sem11;
+
+class Base
+{
+    public virtual String Method()
+    {
+        return "base";
+    }
+}
+
+
+class Derived:Base
+{
+//     public override String Method()
+//     {
+//         return "derived";
+//     }
+
+    public new string Method()
+    {
+        return "derived";
+    }
+    
+}
+
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Base b = new Derived();
+        Derived d = new Derived();
+
+        Console.WriteLine(b.Method());
+        Console.WriteLine(d.Method());
+    }
+}
--- Semestrul III/PLF/LISP/lab2.lisp
+++ Semestrul III/PLF/LISP/lab2.lisp
@@ -1,36 +1,65 @@
-;extract(l: list, stg: list, dr: list, k: integer, lim: integer)
-;extrage subarborii dintr-o lista in doua liste, corespunzatoare tipului de subarbore (stang/drept)
+; Se da un arbore de tipul (1). Sa se precizeze nivelul pe care apare un nod
+; x in arbore. Nivelul radacii se considera a fi 0. 
 
-(defun extract (l stg dr k lim)
-  (cond
-    ((null l) (list stg dr))
-    ((= lim -1) (extract (cddr l) (cons (cadr l) (car l)) dr 0 (cadr l)))
-    ((< k lim) (extract (cddr l) (cons (cadr l) (cons (car l) stg)) dr (+ k 1) lim))
-    ((= k lim) (extract (cddr l) stg (cons (cadr l) (cons (car l) dr)) k lim))))
+;isNod(l: list, e: atom)
+;verifica daca e se afla in l
+(defun isNod (l e)
+    (cond 
+        ((null l) nil)
+        ((eq (car l) e) t)
+        (t (isNod (cddr l) e ))
+    )
+)
 
-(defun INVERS (L)
-  (COND
-    ((ATOM L) L)
-    (T (APPEND (INVERS (CDR L)) (LIST (CAR L))))))
+;runStang(l: list, nivel: integer, n: integer)
+;l - arbore
+;nivel - nivel curent
+;n - adancime nod curent
+; parcurg subarborele stang si construiesc o lista noua a acestui subarbore
 
-
-(defun parcurgere (l, x, nivel)
+(defun runStang(l nivel n)
   (cond
     ((null l) nil)
-    ((eql (caar lista) x) nivel)
-    (t (or (parcurgere (remove-if-not #'listp (cdar lista)) x (1+ nivel))
-           (parcurgere (cdr lista) x nivel)))))
+    ((= nivel (+ 1 n)) nil)
+    ;adaug primul si al doilea element la lista curenta
+    (t (cons (car l) (cons (cadr l) (runStang (cddr l) (+ 1 nivel) (+ (cadr l) n)))))
+  )
+)
 
-; cadr - al doilea elem
-; cddr - out primele 2 elem
-;(defun main (arbore x)
-;   (cond
-;	((null arbore) )
-;	((= x (car arbore) 0)
-;	((= (cadr arbore) 1) parcurgere((cddr arbore) x nivel)
-;	(T (progn  
-;		(setq stg '())
-;		(setq dr '())
-;		(extract(cddr(arbore) stg dr 0 -1))
-;		(invers stg)
-;		(invers dr)
+
+;runDrept(l: list, nivel: integer, n: integer)
+;l - arbore
+;nivel - nivel curent
+;n - adancime nod curent
+; parcurg subarborele drept si construiesc o lista noua a acestui subarbore
+
+(defun runDrept(l nivel n)
+  (cond
+    ((null l) nil)
+    ((= nivel (+ 1 n)) l) ;nivel = adancime+1
+    (t (runDrept (cddr l) (+ 1 nivel) (+ (cadr l) n)))
+  )
+)
+
+;functie apel pt parcurgere
+(defun stang(l)
+  (runStang (cddr l) 0 0)
+)
+
+;functie apel pt parcurgere
+(defun drept(l)
+  (runDrept (cddr l) 0 0)
+)
+
+;nivel(l: list, e: atom)
+;parcurge arborele recursiv pentru a gasi nivelul nodului e
+(defun nivel(l e)
+  (cond
+    ((null l) nil) ;conditie arbore nul
+    ((eq (car l) e) 0)
+    ((isNod(stang l) e ) (+ 1 (nivel (stang l) e))) ;daca se gaseste in subarborele stang
+    (t (+ 1 (nivel (drept l) e)))
+  )
+)
+
+(print (nivel '(a 2 b 1 d 1 e 1 f 2 g 0 h 2 j 0 k 0 c 2 l 2 n 0 q 2 p 0 r 0 m 0) 'r))
