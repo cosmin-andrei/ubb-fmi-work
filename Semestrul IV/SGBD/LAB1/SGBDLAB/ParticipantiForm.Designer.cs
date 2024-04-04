9a053a05acdce1fd91bc20b1cce48bc6a6baa98a
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2024-04-04 11:17:18 +0300

    updates

---
+++ Semestrul IV/SGBD/LAB1/SGBDLAB/ParticipantiForm.Designer.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SGBDLAB
+{
+    partial class ParticipantiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.labelParent = new System.Windows.Forms.Label();
+            this.labelChild = new System.Windows.Forms.Label();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.textBoxEventID = new System.Windows.Forms.TextBox();
+            this.textBoxParticipantName = new System.Windows.Forms.TextBox();
+            this.textBoxParticipantPhone = new System.Windows.Forms.TextBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.labelEventID = new System.Windows.Forms.Label();
+            this.labelParticipantName = new System.Windows.Forms.Label();
+            this.labelParticipantPhone = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(13, 65);
+            this.dataGridViewParent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowHeadersWidth = 51;
+            this.dataGridViewParent.Size = new System.Drawing.Size(818, 257);
+            this.dataGridViewParent.TabIndex = 0;
+            this.dataGridViewParent.SelectionChanged += new System.EventHandler(this.dataGridViewParent_SelectionChanged);
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(13, 385);
+            this.dataGridViewChild.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowHeadersWidth = 51;
+            this.dataGridViewChild.Size = new System.Drawing.Size(818, 257);
+            this.dataGridViewChild.TabIndex = 1;
+            this.dataGridViewChild.SelectionChanged += new System.EventHandler(this.dataGridViewChild_SelectionChanged);
+            // 
+            // labelParent
+            // 
+            this.labelParent.AutoSize = true;
+            this.labelParent.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelParent.ForeColor = System.Drawing.Color.Firebrick;
+            this.labelParent.Location = new System.Drawing.Point(13, 28);
+            this.labelParent.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelParent.Name = "labelParent";
+            this.labelParent.Size = new System.Drawing.Size(89, 32);
+            this.labelParent.TabIndex = 2;
+            this.labelParent.Text = "Events";
+            // 
+            // labelChild
+            // 
+            this.labelChild.AutoSize = true;
+            this.labelChild.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelChild.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.labelChild.Location = new System.Drawing.Point(13, 348);
+            this.labelChild.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelChild.Name = "labelChild";
+            this.labelChild.Size = new System.Drawing.Size(151, 32);
+            this.labelChild.TabIndex = 3;
+            this.labelChild.Text = "Participants";
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonRefresh.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.buttonRefresh.Location = new System.Drawing.Point(1116, 385);
+            this.buttonRefresh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(138, 35);
+            this.buttonRefresh.TabIndex = 4;
+            this.buttonRefresh.Text = "Refresh Data";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            // 
+            // textBoxEventID
+            // 
+            this.textBoxEventID.Location = new System.Drawing.Point(956, 65);
+            this.textBoxEventID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxEventID.Name = "textBoxEventID";
+            this.textBoxEventID.Size = new System.Drawing.Size(298, 26);
+            this.textBoxEventID.TabIndex = 0;
+            // 
+            // textBoxParticipantName
+            // 
+            this.textBoxParticipantName.Location = new System.Drawing.Point(956, 127);
+            this.textBoxParticipantName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxParticipantName.Name = "textBoxParticipantName";
+            this.textBoxParticipantName.Size = new System.Drawing.Size(298, 26);
+            this.textBoxParticipantName.TabIndex = 1;
+            // 
+            // textBoxParticipantPhone
+            // 
+            this.textBoxParticipantPhone.Location = new System.Drawing.Point(956, 189);
+            this.textBoxParticipantPhone.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxParticipantPhone.Name = "textBoxParticipantPhone";
+            this.textBoxParticipantPhone.Size = new System.Drawing.Size(298, 26);
+            this.textBoxParticipantPhone.TabIndex = 2;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonDelete.ForeColor = System.Drawing.Color.Black;
+            this.buttonDelete.Location = new System.Drawing.Point(956, 236);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(140, 35);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click_1);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonUpdate.ForeColor = System.Drawing.Color.Black;
+            this.buttonUpdate.Location = new System.Drawing.Point(1104, 236);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(150, 35);
+            this.buttonUpdate.TabIndex = 4;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click_1);
+            // 
+            // labelEventID
+            // 
+            this.labelEventID.AutoSize = true;
+            this.labelEventID.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelEventID.Location = new System.Drawing.Point(839, 61);
+            this.labelEventID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelEventID.Name = "labelEventID";
+            this.labelEventID.Size = new System.Drawing.Size(93, 28);
+            this.labelEventID.TabIndex = 5;
+            this.labelEventID.Text = "Event ID:";
+            // 
+            // labelParticipantName
+            // 
+            this.labelParticipantName.AutoSize = true;
+            this.labelParticipantName.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelParticipantName.Location = new System.Drawing.Point(861, 123);
+            this.labelParticipantName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelParticipantName.Name = "labelParticipantName";
+            this.labelParticipantName.Size = new System.Drawing.Size(71, 28);
+            this.labelParticipantName.TabIndex = 6;
+            this.labelParticipantName.Text = "Name:";
+            // 
+            // labelParticipantPhone
+            // 
+            this.labelParticipantPhone.AutoSize = true;
+            this.labelParticipantPhone.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelParticipantPhone.Location = new System.Drawing.Point(856, 185);
+            this.labelParticipantPhone.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelParticipantPhone.Name = "labelParticipantPhone";
+            this.labelParticipantPhone.Size = new System.Drawing.Size(76, 28);
+            this.labelParticipantPhone.TabIndex = 7;
+            this.labelParticipantPhone.Text = "Phone:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonAdd.ForeColor = System.Drawing.Color.ForestGreen;
+            this.buttonAdd.Location = new System.Drawing.Point(956, 284);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(298, 35);
+            this.buttonAdd.TabIndex = 8;
+            this.buttonAdd.Text = "Add participant";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click_1);
+            // 
+            // ParticipantiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 675);
+            this.Controls.Add(this.textBoxEventID);
+            this.Controls.Add(this.textBoxParticipantName);
+            this.Controls.Add(this.textBoxParticipantPhone);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.labelEventID);
+            this.Controls.Add(this.labelParticipantName);
+            this.Controls.Add(this.labelParticipantPhone);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.labelChild);
+            this.Controls.Add(this.labelParent);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ParticipantiForm";
+            this.Text = "ParticipantiForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.Label labelParent;
+        private System.Windows.Forms.Label labelChild;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TextBox textBoxEventID;
+        private System.Windows.Forms.TextBox textBoxParticipantName;
+        private System.Windows.Forms.TextBox textBoxParticipantPhone;
+
+        // Declarațiile pentru butoane
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelEventID;
+        private System.Windows.Forms.Label labelParticipantName;
+        private System.Windows.Forms.Label labelParticipantPhone;
+    }
+
+
+}
+
---
+++ Semestrul IV/SGBD/LAB1/SGBDLAB/ParticipantiForm.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SGBDLAB
+{
+    public partial class ParticipantiForm : Form
+    {
+        string _connectionString = @"Server=DESKTOP-COR\SQLEXPRESS10; Database=Organizatie;
+        Integrated Security=true;TrustServerCertificate=true;";
+
+        //adaptor = interogari sql, preluam date din DB
+        DataSet _ds = new DataSet();
+        SqlDataAdapter _parentAdapter;
+        SqlDataAdapter _childAdapter;
+        BindingSource _bsParent;
+        BindingSource _bsChild;
+
+        public ParticipantiForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //deschid conexiunea cu DB
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+                    // MessageBox.Show("Starea conexiunii: " + conn.State.ToString());
+
+                    //initializare adaptoare pt tabele
+                    //adaptor = interogari sql, preluam date din DB
+                    _parentAdapter = new SqlDataAdapter("SELECT * FROM Eveniment;", conn);
+                    _childAdapter = new SqlDataAdapter("SELECT * FROM Participant;", conn);
+                    _parentAdapter.Fill(_ds, "Evenimente");
+                    _childAdapter.Fill(_ds, "Participanti");
+
+                    //definesc relatia dintre tabele (PK, FK)
+                    DataColumn pkColumn = _ds.Tables["Evenimente"].Columns["id_eveniment"];
+                    DataColumn fkColumn = _ds.Tables["Participanti"].Columns["id_eveniment"];
+                    DataRelation relation = new DataRelation("FK_Evenimente_Angajati", pkColumn, fkColumn);
+                    _ds.Relations.Add(relation);
+
+                    //BindingSource = conectam datele intre UI (DataGridView) si Dataset
+                    _bsParent = new BindingSource();
+                    _bsChild = new BindingSource();
+                    _bsParent.DataSource = _ds.Tables["Evenimente"];
+                    dataGridViewParent.DataSource = _bsParent;
+                    _bsChild.DataSource = _bsParent;
+                    _bsChild.DataMember = "FK_Evenimente_Angajati";
+                    dataGridViewChild.DataSource = _bsChild;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonRefresh_Click()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    //apartine SQLDATAADAPTER - operatiune de refresh date pt ca s-a actualizat baza => update conexiune
+                    _parentAdapter.SelectCommand.Connection = conn;
+                    _childAdapter.SelectCommand.Connection = conn;
+
+                    _ds.Tables["Participanti"].Clear();
+                    _ds.Tables["Evenimente"].Clear();
+
+                    _parentAdapter.Fill(_ds, "Evenimente");
+                    _childAdapter.Fill(_ds, "Participanti");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonDelete_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridViewChild.SelectedRows.Count == 0)
+            {
+                MessageBox.Show(@"Selecteaza un participant!");
+            }
+            else if (dataGridViewChild.SelectedRows.Count > 1)
+            {
+                MessageBox.Show(@"Un singur participant!");
+            }
+            else
+                try
+                {
+                    //extragere rand + id
+                    DataGridViewRow selectedRow = dataGridViewChild.CurrentRow;
+                    if (selectedRow != null)
+                    {
+                        int participantId = (int)selectedRow.Cells["id_participant"].Value;
+
+                        using (SqlConnection conn = new SqlConnection(_connectionString))
+                        {
+                            conn.Open();
+                            SqlCommand cmd = new SqlCommand("DELETE FROM Participant WHERE id_participant = @participantId",
+                                conn);
+                            cmd.Parameters.AddWithValue("@participantId", participantId);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    textBoxParticipantName.Text = "";
+                    textBoxParticipantPhone.Text = "";
+                    buttonRefresh_Click();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+
+        private void buttonUpdate_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridViewChild.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show(@"Selectati un participant");
+                    return;
+                }
+
+                DataGridViewRow selectedRow = dataGridViewChild.SelectedRows[0];
+                int participantId = (int)selectedRow.Cells["id_participant"].Value;
+
+                string updatedParticipantName = textBoxParticipantName.Text;
+                string updatedParticipantPhone = textBoxParticipantPhone.Text;
+
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd =
+                        new SqlCommand(
+                            "UPDATE Participant SET nume_participant = @participantName, telefon_participant = @participantPhone WHERE id_participant = @participantId",
+                            conn);
+                    cmd.Parameters.AddWithValue("@participantName", updatedParticipantName);
+                    cmd.Parameters.AddWithValue("@participantPhone", updatedParticipantPhone);
+                    cmd.Parameters.AddWithValue("@participantId", participantId);
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show(@"Participant actualizat!");
+                textBoxParticipantName.Text = "";
+                textBoxParticipantPhone.Text = "";
+                buttonRefresh_Click();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonAdd_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBoxEventID.Text))
+                {
+                    MessageBox.Show(@"Selecteaza un eveniment");
+                    return;
+                }else if(string.IsNullOrWhiteSpace(textBoxParticipantName.Text) || string.IsNullOrWhiteSpace(textBoxParticipantPhone.Text))
+                {
+                    MessageBox.Show(@"Completati toate campurile!");
+                    return;
+                }
+
+                int evenimentId = int.Parse(textBoxEventID.Text);
+                string participantName = textBoxParticipantName.Text;
+                string participantPhone = textBoxParticipantPhone.Text;
+
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd =
+                        new SqlCommand(
+                            "INSERT INTO Participant (id_eveniment, nume_participant, telefon_participant) VALUES (@evenimentId, @participantName, @participantPhone)",
+                            conn);
+                    cmd.Parameters.AddWithValue("@evenimentId", evenimentId);
+                    cmd.Parameters.AddWithValue("@participantName", participantName);
+                    cmd.Parameters.AddWithValue("@participantPhone", participantPhone);
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show(@"Participant adaugat");
+                textBoxParticipantName.Text = "";
+                textBoxParticipantPhone.Text = "";
+                buttonRefresh_Click();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void dataGridViewChild_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridViewChild.SelectedRows.Count == 1)
+            {
+                DataGridViewRow selectedRow = dataGridViewChild.SelectedRows[0];
+
+                string participantName = selectedRow.Cells["nume_participant"].Value.ToString();
+                string participantPhone = selectedRow.Cells["telefon_participant"].Value.ToString();
+
+                textBoxParticipantName.Text = participantName;
+                textBoxParticipantPhone.Text = participantPhone;
+            }
+        }
+
+        private void dataGridViewParent_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridViewParent.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridViewParent.SelectedRows[0];
+
+
+                int evenimentId = (int)selectedRow.Cells["id_eveniment"].Value;
+
+                textBoxEventID.Text = evenimentId.ToString();
+                textBoxParticipantName.Text = "";
+                textBoxParticipantPhone.Text = "";
+            }
+        }
+    }
+}
